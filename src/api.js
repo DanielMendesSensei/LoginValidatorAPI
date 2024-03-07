dd73c7b5f83783a2e4422822e6bf530e1f0c869a
Daniel Mendes <senseidanielmendes@gmail.com>
2024-03-07 19:30:59 -0300

    test: remove CORS

--- src/api.js
+++ src/api.js
@@ -10,14 +10,14 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-const corsOptions = {
-  origin: "",
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "",
+//   optionsSuccessStatus: 200,
+// };
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-app.use(cors());
+// app.use(cors());
 
 app.get("/", (req, res) => res.json({ message: "Hello World!" }));
 
